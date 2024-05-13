2dea4193726409fe21392834ab41826b98af5e5b
Aleksey Anikeev <90957150+AlekseiAnikeev@users.noreply.github.com>
2024-05-13 21:32:18 +0300

    refactor

--- Movement.cs
+++ Movement.cs
@@ -1,24 +1,24 @@
 using UnityEngine;
 
-public class GoPlaces : MonoBehaviour
+public class Movement : MonoBehaviour
 {
     [SerializeField] private Transform _pointContainer;
     [SerializeField] private float _speed;
 
     private Transform[] _points;
-    private int _currentPoint;
+    private int _currentPointIndex;
 
     private void Start()
     {
         _points = new Transform[_pointContainer.childCount];
 
-        for (int i = 0; i < _pointContainer.childCount; i++)
-            _points[i] = _pointContainer.GetChild(i).GetComponent<Transform>();
+        for (int i = 0; i < _points.Length; i++)
+            _points[i] = _pointContainer.GetChild(i);
     }
 
     private void Update()
     {
-        var target = _points[_currentPoint];
+        Transform target = _points[_currentPointIndex];
 
         transform.position =
             Vector3.MoveTowards(transform.position, target.position, _speed * Time.deltaTime);
@@ -29,9 +29,6 @@ public class GoPlaces : MonoBehaviour
 
     private void ChangeNextPlace()
     {
-        _currentPoint++;
-
-        if (_currentPoint == _points.Length)
-            _currentPoint = 0;
+        _currentPointIndex = ++_currentPointIndex % _points.Length;
     }
 }
--- SpawnerBullet.cs
+++ SpawnerBullet.cs
@@ -1,23 +1,26 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class InstantiateBulletsShooting : MonoBehaviour
+public class SpawnerBullet : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private float _cooldown;
     [SerializeField] private Rigidbody _prefab;
-    [SerializeField] private Transform _objectToShoot;
+    [SerializeField] private Transform _target;
 
     private void Start() => StartCoroutine(nameof(ShootingWorker));
 
     private IEnumerator ShootingWorker()
     {
+        WaitForSecondsRealtime waitingTime = new WaitForSecondsRealtime(_cooldown);
+
         while (enabled)
         {
-            yield return new WaitForSecondsRealtime(_cooldown);
-            
-            var direction = (_objectToShoot.position - transform.position).normalized;
-            var newBullet = Instantiate(_prefab, transform.position + direction, Quaternion.identity);
+            yield return waitingTime;
+
+            Vector3 direction = (_target.position - transform.position).normalized;
+            Rigidbody newBullet = Instantiate(_prefab, transform.position + direction, Quaternion.identity);
 
             newBullet.transform.up = direction;
             newBullet.velocity = direction * _speed;
